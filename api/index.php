0c1689075df2e01836b8ae0d382a66c601319d93
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-24 15:43:30 -0300

    🚧 build: Gerando o index da API

--- api/index.php
+++ api/index.php
@@ -1,8 +1,10 @@
 <?php 
+    // Variavel armazena o resultado da API
     $data = [];
 
     if (isset($_GET['option'])) {
         
+        // Verificando se contem o status da API
         switch ($_GET['option']) {
             case 'status':
                 $data['status'] = 'SUCCESS';
@@ -14,13 +16,16 @@
                 break;
         }
     } else {
+        // Retornando erro caso a variavel nao contenha nenhum status
         $data['status'] = 'ERROR';
     }
 
+    // Função de retorno da API
     function response($data_response) {
-        header("Content-Type:application/json");
-        echo json_encode($data_response);
+        header("Content-Type:application/json"); // Informando que o padrão de retorno desta página é json, e não html.
+        echo json_encode($data_response); // Convertendo a variavel data para json e exibindo
     }
 
+    // Chamando a função de retorno da API
     response($data);
 ?>
