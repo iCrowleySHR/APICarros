55a666ee984c9fccc9c532d54cc488e737b29a96
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-10 23:03:44 -0300

    ➕ feat(DOCS) Adicionando inserções e consultas no banco!

---
+++ DOCS/mysql/DML - consulta.sql
@@ -0,0 +1,11 @@
+use api_carros;
+
+select * from marca;
+
+select * from modelo;
+
+select * from combustivel;
+
+select * from transmissao;
+
+select * from veiculo;
---
+++ DOCS/mysql/DML - inserções.sql
@@ -0,0 +1,41 @@
+use api_carros;
+
+insert into combustivel (combustivel) values
+('Gasolina'),
+('Etanol'),
+('Diesel'),
+('Gás Natural'),
+('Elétrico'),
+('Hidrogênio');
+
+insert into transmissao (transmissao) values
+('Manual'), 
+('Automática'),
+('Automatizada'),
+('Continuamente variável');
+
+insert into marca (nome_marca) values 
+('Audi'),
+('Bmw'),
+('Chevrolet'),
+('Citroen'),
+('Fiat'),
+('Ford'),
+('Gurgel'),
+('Honda'),
+('Hyundai'),
+('JAC'),
+('Jeep'),
+('KIA'),
+('Land Rover'),
+('Mercedes-Benz'),
+('Mitsubishi'),
+('Nissan'),
+('Peugeot'),
+('Puma'),
+('Renault'),
+('Subaru'),
+('Suzuki'),
+('Toyota'),
+('Volvo'),
+('Volkswagen');
